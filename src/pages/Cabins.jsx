0a3ee3586bc3848b27f83bebb1bc40d71985c344
esJohan <johanchaconchacon@gmail.com>
2025-07-25 00:29:19 -0600

    set the Supabase

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,11 +1,22 @@
+import { useEffect, useState } from "react";
+
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
+import { getCabins } from "../services/apiCabins";
 
 function Cabins() {
+  const [cabins, setCabins] = useState([]);
+  useEffect(() => {
+    function fetchCabins() {
+      setCabins(getCabins());
+    }
+    fetchCabins();
+  }, [cabins]);
   return (
     <Row type="horizontal">
       <Heading as="h1">All cabins</Heading>
       <p>TEST</p>
+      <img src="https://smrkcdgckgdtwuhoebml.supabase.co/storage/v1/object/public/cabin-images//cabin-001.jpg" />
     </Row>
   );
 }
---
+++ src/services/apiCabins.js
@@ -0,0 +1,10 @@
+import supabase from "./supabase";
+
+export async function getCabins() {
+  const { data, error } = await supabase.from("cabins").select("*");
+  if (error) {
+    console.error("Cabins could not be loaded", error);
+    throw new Error(error.message);
+  }
+  return data;
+}
